3d598162e64547e3695bc16f5413de707e728c5b
Abigail <4estgirl322@gmail.com>
2021-12-17 16:55:24 -0500

    Made logic for user prompt, and randomly generating the computer's choice

---
+++ script.js
@@ -0,0 +1,20 @@
+//Prompt user to input Rock Paper or Scissors
+let userInput = prompt("Make your choice!", )
+//Convert user input to all caps
+let userChoice = userInput.toUpperCase()
+console.log(userChoice)
+//Display userChoice
+//Computer randomly chooses a number
+let number = Math.random() * 100
+let computerChoice = "NONE CHOSEN"
+if (number <= 50) {
+    computerChoice =  "ROCK";
+} else if (number >= 51 || number <=75) {
+    computerChoice =  "PAPER";
+} else {computerChoice =  "SCISSORS";}
+console.log(computerChoice)
+//User selection is compared to Computer selection to determine winner of round
+//Winner of round is announced
+//Overall game score is updated
+//Loop 5 times
+//After the 5th round, the grand champion is announced
